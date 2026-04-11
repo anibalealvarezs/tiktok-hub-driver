3acc806cdc06c45665363977c25c1f3e3f41285f
Anibal Alvarez <aalvarez@chmw.co>
2026-04-11 16:23:42 -0400

    Implement mandatory getChannelIcon() method

--- src/Drivers/TikTokDriver.php
+++ src/Drivers/TikTokDriver.php
@@ -45,6 +45,16 @@ class TikTokDriver implements SyncDriverInterface
     }
 
     /**
+     * Get the display icon for the channel.
+     * 
+     * @return string
+     */
+    public static function getChannelIcon(): string
+    {
+        return 'TT';
+    }
+
+    /**
      * Get the routes served by this driver.
      * 
      * @return array
